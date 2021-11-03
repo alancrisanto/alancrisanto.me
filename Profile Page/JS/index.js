9a8d5ae858c2343e21b4bcf68661421aff106b1c
Alan Crisanto <alanvcrisanto@gmail.com>
2021-11-02 22:13:39 -0500

    Adding alt to img

--- Profile Page/JS/index.js
+++ Profile Page/JS/index.js
@@ -1,33 +1,3 @@
-// let daysNames = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday"
-// ];
-
-// let months = [
-//     "January",
-//     "February",
-//     "March",
-//     "April",
-//     "May",
-//     "June",
-//     "July",
-//     "August",
-//     "September",
-//     "October",
-//     "November",
-//     "December",
-// ];
-
-// let d = new Date();
-
-// let dayName = daysNamesNames[d.getDay()];
-// let monthName = months[d.getMonth()]
-
 try {
     let options = {
         weekday: "long",
